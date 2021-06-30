c817cbe25302d002bfc102ad898f3924e1f64e3c
swonb <47047914+swonb@users.noreply.github.com>
2021-06-29 23:52:33 -0400

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>QiziGram</h1>
+      <h2>Your Picture Album</h2>
+      <p>Upload your image files</p>
+    </div>
+  )
+}
+
+export default Title;
